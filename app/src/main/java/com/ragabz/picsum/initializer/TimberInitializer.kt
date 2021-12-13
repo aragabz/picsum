e58f7e87e314b1e822bef0eb00b126f98561bb8a
ahmeragab <94692067+ahmeragab@users.noreply.github.com>
2021-12-13 23:46:08 +0200

    first commit

---
+++ app/src/main/java/com/ragabz/picsum/initializer/TimberInitializer.kt
@@ -0,0 +1,17 @@
+package com.ragabz.picsum.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.ragabz.githubapp.BuildConfig
+import timber.log.Timber
+
+class TimberInitializer : Initializer<Unit> {
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        Timber.d("TimberInitializer --> initialized")
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> = mutableListOf()
+}
